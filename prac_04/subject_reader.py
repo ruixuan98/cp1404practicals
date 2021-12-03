13ce76268e98e3eb4c27333a782ca7f1c0430989
ruixu <ruixuan.cheng@my.jcu.edu.au>
2021-12-03 13:34:11 +0800

    Add function to get data and display details

---
+++ prac_04/subject_reader.py
@@ -0,0 +1,39 @@
+"""
+CP1404/CP5632 Practical
+Data file -> lists program
+"""
+
+FILENAME = "subject_data.txt"
+
+
+def main():
+    data = get_data()
+    print(data)
+    display_details(data)
+
+
+def get_data():
+    """Read data from file formatted like: subject,lecturer,number of students."""
+    input_file = open(FILENAME)
+    data = []
+    for line in input_file:
+        print(line)  # See what a line looks like
+        print(repr(line))  # See what a line really looks like
+        line = line.strip()  # Remove the \n
+        parts = line.split(',')  # Separate the data into its parts
+        print(parts)  # See what the parts look like (notice the integer is a string)
+        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
+        print(parts)  # See if that worked
+        print("----------")
+        data.append(parts)
+    return data
+
+    input_file.close()
+
+
+def display_details(subjects):
+    for line in subjects:
+        print(f"{line[0]} is taught by {line[1]} and has {line[2]} students.")
+
+
+main()
