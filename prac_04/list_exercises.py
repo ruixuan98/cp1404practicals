cab1efd304e547b6e5ba1e6862b76f38c0e3a811
Rui Xuan <ruixuan.cheng@my.jcu.edu.au>
2021-12-04 16:08:27 +0800

    Add function to ask user to input username and grant access if username is valid

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -13,3 +13,10 @@ numbers.sort()
 print(f"The smallest number is {numbers[0]}")
 print(f"The largest number is {numbers[-1]}")
 print(f"The average of the numbers is: {sum(numbers)/len(numbers)}")
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Enter your username: ")
+while username not in usernames:
+    print("Access denied.")
+    username = input("Enter your username: ")
+print("Access granted")
