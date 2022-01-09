147fc540c44ab9733b90fb55a1f2789e297738f4
ruixuan98 <ruixuan90@gmail.com>
2022-01-09 20:33:47 +0800

    Initial commit

---
+++ prac_08/taxi_simulator.py
@@ -0,0 +1,46 @@
+from prac_08.silver_service_taxi import SilverServiceTaxi
+from prac_08.car import Car
+from prac_08.taxi import Taxi
+
+"""
+CP1404 Practicals
+Taxi simulator to allow users to choose which taxi they want
+"""
+
+MENU = """q)uit, c)hoose taxi, d)rive"""
+
+def main():
+    """Get user input and drive taxi"""
+    current_taxi = None
+    current_bill = 0
+    total_bill = 0
+    print("Let's drive!")
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    print(MENU)
+    user_option = input(">>> ").lower()
+    while user_option != "q":
+        if user_option == "c":
+            print("Taxis available:")
+            list_taxis(taxis)
+            taxi_choice = int(input("Choose taxi: "))
+            user_option = input(">>> ").lower()
+            try:
+                current_taxi = taxis[taxi_choice]
+            except IndexError:
+                print("Invalid taxi choice")
+        elif user_option == "d":
+            if current_taxi:
+                distance = float(input("Drive how far? "))
+                current_taxi.start_fare()
+                current_bill = current_taxi.drive(distance)
+                print(f"Bill to date: {current_bill}")
+                print(current_taxi)
+
+
+
+def list_taxis(taxis):
+    for i, taxi in enumerate(taxis):
+        print(f"{i} - {taxi}")
+
+
+main()
