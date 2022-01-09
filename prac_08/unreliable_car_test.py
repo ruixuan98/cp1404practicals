8b77b834def725e83ea2b6fff5c1db3e3caf0bad
ruixuan98 <ruixuan90@gmail.com>
2022-01-09 20:52:15 +0800

    Complete program

--- prac_08/unreliable_car_test.py
+++ prac_08/unreliable_car_test.py
@@ -4,5 +4,5 @@ from prac_08.unreliable_car import UnreliableCar
 from prac_08.car import Car
 
 unreliable_car = UnreliableCar("CartyMccarface", 100, 50)
-unreliable_car.drive(20)
+unreliable_car.drive(50)
 print(unreliable_car)
