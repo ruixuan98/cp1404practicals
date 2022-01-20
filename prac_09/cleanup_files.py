27dc548164b111fb47e4736ea627f60c43e3220c
ruixuan98 <ruixuan90@gmail.com>
2022-01-20 10:25:13 +0800

    Initial commit

---
+++ prac_09/cleanup_files.py
@@ -0,0 +1,55 @@
+"""
+CP1404 practicals
+Name lyrics files to the same format
+"""
+import os
+
+def main():
+    """Demo os module functions."""
+    print("Starting directory is: {}".format(os.getcwd()))
+
+    os.chdir('Lyrics/Christmas')
+
+    print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
+
+    try:
+        os.mkdir('temp')
+    except FileExistsError:
+        pass
+
+    # Loop through each file in the (current) directory
+    for filename in os.listdir('.'):
+        # Ignore directories, just process files
+        if os.path.isdir(filename):
+            continue
+
+        new_name = get_fixed_filename(filename)
+        print("Renaming {} to {}".format(filename, new_name))
+
+        # Option 1: rename file to new name - in place
+        # os.rename(filename, new_name)
+
+        # Option 2: move file to new place, with new name
+        # shutil.move(filename, 'temp/' + new_name)
+
+def get_fixed_filename(filename):
+    """Return a 'fixed' version of filename."""
+    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+    return new_name
+
+def demo_walk():
+    """Process all subdirectories using os.walk()."""
+    os.chdir('Lyrics')
+    for directory_name, subdirectories, filenames in os.walk('.'):
+        print("Directory:", directory_name)
+        print("\tcontains subdirectories:", subdirectories)
+        print("\tand files:", filenames)
+        print("(Current working directory is: {})".format(os.getcwd()))
+
+        for filename in filenames:
+            full_name = os.path.join(directory_name, filename)
+            new_name = os.path.join(directory_name, get_fixed_filename(filename))
+            os.rename(full_name, new_name)
+
+main()
+# demo_walk()
