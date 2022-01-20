ae2a63031d144e08eaffab1767e2ac309e4b30af
ruixuan98 <ruixuan90@gmail.com>
2022-01-20 11:00:50 +0800

    Add variable file_extension to check if filename is split properly

--- prac_09/sort_files_1.py
+++ prac_09/sort_files_1.py
@@ -4,13 +4,23 @@ Sort files into directory
 """
 
 import os
+import shutil
 
 def main():
     os.chdir("FilesToSort")
+    print(os.getcwd())
 
-    for filename in os.getcwd():
+    for filename in os.listdir('.'):
         extension = filename.split('.')
+        file_extension = extension[1]
+        print(file_extension)
 
+    try:
+        os.mkdir(extension)
+    except FileExistsError:
+        pass
+    os.rename(filename, f"{file_extension},{filename}")
+    shutil.move(filename, extension)
 
 
 
